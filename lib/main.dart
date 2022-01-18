3ff4383f16eaf48118588ba5a8a0fb39972acc55
ridatuljannah0112 <ridhaatulljannah12@gmail.com>
2022-01-18 16:33:06 +0800

    melakukan perubahan di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:aplikasi_berita_ridatuljannah_0009/services/Splashscreen_view.dart';
+import 'package:aplikasi_berita_ridatuljannah_0009/Splashscreen_view.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_berita_ridatuljannah_0009/components/customListTile.dart';
 import 'package:aplikasi_berita_ridatuljannah_0009/models/article_model.dart';
 import 'package:aplikasi_berita_ridatuljannah_0009/services/api_service.dart';
+import 'package:aplikasi_berita_ridatuljannah_0009/services/navigation.dart';
+import 'package:aplikasi_berita_ridatuljannah_0009/profile/profile.dart';
 
 
 void main() {
@@ -10,28 +12,65 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
+        return MaterialApp(
+        home: SplashScreen(),
+        routes: <String, WidgetBuilder> {
+        'HalamanPertama': (BuildContext context) => new HalamanPertama(),
+        'bisnis': (BuildContext context) => new HomePage1(),
+        'sport': (BuildContext context) => new HomePage2(),
+        'profile': (BuildContext context) => new profile(),
+      },
     );
   }
-} 
-
-class HomePage extends StatefulWidget{
-  @override
-  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  ApiService client = ApiService();
+
+class HomePage1 extends StatefulWidget{
+  @override
+  berita_bisnis createState() => berita_bisnis();
+}
+
+class HomePage2 extends StatefulWidget{
+  berita_sport createState() => berita_sport();
+}
+
+class berita_bisnis extends State<HomePage1> {
+  ApiService_bisnis client = ApiService_bisnis();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("News App", style: TextStyle(color: Colors.black)),
+        title: Text("Berita Bisnis", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white),
+      body: FutureBuilder(
+        future: client.getArticle(),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot)
+{
+          if (snapshot.hasData) {
+            List<Article> article = snapshot.data;
+            return ListView.builder(itemBuilder: (context, index) => 
+                customListTile(article[index], context));
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+},
+      ),
+      );
+  }
+}
+
+class berita_sport extends State<HomePage2> {
+  ApiService_sport client = ApiService_sport();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Berita Sport", style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white),
       body: FutureBuilder(
         future: client.getArticle(),
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,14 +2,35 @@ import 'dart:convert';
 import 'package:aplikasi_berita_ridatuljannah_0009/models/article_model.dart';
 import 'package:http/http.dart' as http;
 
-class ApiService {
+class ApiService_bisnis {
   final endPointUrl = "newsapi.org";
   final client = http.Client();
 
   Future<List<Article>> getArticle() async {
     final queryParameter = {
       'country': 'id',
-      'category': 'technology',
+      'category': 'business',
+      'apiKey': 'c86b0d07478c489e8ff9fbc2113ddea9'
+    };
+
+    final uri = Uri.https(endPointUrl, '/v2/top-headlines', queryParameter);
+    final response = await client.get(uri);
+    Map<String, dynamic> json = jsonDecode(response.body);
+    List<dynamic> body = json['articles'];
+    List<Article> articles =
+        body.map((dynamic item) => Article.fromJson(item)).toList();
+      return articles;
+  }
+}
+
+class ApiService_sport {
+  final endPointUrl = "newsapi.org";
+  final client = http.Client();
+
+  Future<List<Article>> getArticle() async {
+    final queryParameter = {
+      'country': 'id',
+      'category': 'sport',
       'apiKey': 'c86b0d07478c489e8ff9fbc2113ddea9'
     };
 
